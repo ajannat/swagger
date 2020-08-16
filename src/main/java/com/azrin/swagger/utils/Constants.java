766969557b7b4922baee53a7bee4e12f4651f6d9
Golam Azrin <golam.azrin@dsinnovators.com>
2020-08-16 17:26:37 +0600

    init

---
+++ src/main/java/com/azrin/swagger/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.azrin.swagger.utils;
+
+public class Constants {
+
+    public static final String BASE_API_URL="api/x/v1/";
+    public static final String API_BASE_PACKAGE="com.azrin.swagger.controller";
+
+    /*------------------------------- API Response Style ------------------------------*/
+    public static final String RESPONSE_BODY_DATA = "data";
+    public static final String RESPONSE_BODY_PAGEINFO = "pageInfo";
+    public static final String RESPONSE_BODY_STATUS = "status";
+    public static final String RESPONSE_BODY_MESSAGE = "message";
+    public static final String MESSAGE_SUCCESSFUL = "successful";
+}
